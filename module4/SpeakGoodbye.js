70b2dca3589bb5e16bf1840aca948b7fa5399527
arjundhaka <149366812+arjundhaka@users.noreply.github.com>
2024-01-25 02:27:47 +0530

    Delete module4 directory

--- module4/SpeakGoodbye.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-	var byeSpeaker={};
-	var speakWord="Good Bye";
-	byeSpeaker.speak=function (name) {
-		document.write(speakWord+" "+name+" ");
-	}
-	window.byeSpeaker=byeSpeaker;
-})(window);
--- module4/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-	var helloSpeaker={};
-	var speakWord="Hello";
-	helloSpeaker.speak=function (name) {
-		document.write(speakWord+" "+name+" ");
-	}
-	window.helloSpeaker=helloSpeaker;
-})(window);
